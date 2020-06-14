180310d4a2367372d923ee5bd1d832c2b2d7f598
anpu <33736796+anpu2468@users.noreply.github.com>
2020-06-13 23:26:52 -0400

    experimenting with module code in my main script
    Ive messed around with module code for a long while, so i wonder if the code itself is broken. This should help with figuring that about.

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
-import { generators }  from './js/generators.js';
+//import { generators }  from './js/generators.js';
+let generators = []
+function createGenerators(gen) {
+  for (let i in gen) {
+    let generator = {
+      cost: gen[i][0],
+      production: gen[i][1],
+      costIncrease: gen[i][2],
+      name: gen[i][3],
+      amount: 0
+    }
+    generators.push(generator)
+
+  }
+
+}
+createGenerators([[10, 0.5, 20, "The Intern"]])
 let player = {
   cash: 0,
 }
